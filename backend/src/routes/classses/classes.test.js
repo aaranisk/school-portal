a785c5b83f52e58710e076e99eb1145c59e13648
aaranisk <aaranisk@gmail.com>
2025-09-03 13:34:21 +0800

    test: backend tests for api

---
+++ backend/src/routes/classses/classes.test.js
@@ -0,0 +1,207 @@
+import request from "supertest";
+import express from "express";
+import AppDataSource from "../../datasource.js";
+import ClassEntity from "../../typeorm/entities/class.entity.js";
+import TeacherEntity from "../../typeorm/entities/teacher.entity.js";
+import classes from "./classes.js";
+import {IsNull, Not} from "typeorm";
+
+
+jest.mock("../../typeorm/logger/winston.js", () => ({
+    log: jest.fn()
+}));
+
+let app;
+
+beforeAll(async () => {
+    app = express();
+    app.use(express.json());
+    app.use("/classes", classes);
+    await AppDataSource.initialize();
+    await AppDataSource.getRepository(ClassEntity).delete({id: Not(IsNull())});
+    await AppDataSource.getRepository(TeacherEntity).delete({id: Not(IsNull())});
+});
+
+afterEach(async () => {
+    await AppDataSource.getRepository(ClassEntity).delete({id: Not(IsNull())});
+    await AppDataSource.getRepository(TeacherEntity).delete({id: Not(IsNull())});
+});
+
+describe("Classes API", () => {
+    const teacherRepo = AppDataSource.getRepository(TeacherEntity);
+    const classRepo = AppDataSource.getRepository(ClassEntity);
+    beforeEach(async () => {
+        const teachers = [{
+            name: "Sarah Lim",
+            email: "sarah@gmail.com",
+            subject: "English Language",
+            contactNumber: '68129414'
+        }, {
+            name: "Hannah Low",
+            subject: "Science",
+            contactNumber: '68129415',
+            email: "hannahloh@gmail.com"
+        }]
+        await teacherRepo.save(teachers);
+    });
+    describe("GET /api/classes", () => {
+        describe("returns 200 status code ", () => {
+            test("and an empty array when no classes are saved to the db", async () => {
+                const res = await request(app).get("/classes");
+                expect(res.status).toBe(200);
+                expect(res.body).toEqual({data: []});
+            });
+
+            test("returns array of classes when classes are already saved in the db", async () => {
+                const classes = [
+                    {
+                        level: 'Primary 2',
+                        name: "Primary 2 English",
+                        teacherEmail: "sarah@gmail.com"
+                    }, {
+                        level: 'Primary 3',
+                        name: "Primary 3 Science",
+                        teacherEmail: "hannahloh@gmail.com"
+                    }]
+                await classRepo.save(classes);
+                const res = await request(app).get("/classes");
+                expect(res.status).toBe(200);
+                expect(res.body).toEqual({
+                    data: [{
+                        level: 'Primary 2',
+                        name: "Primary 2 English",
+                        formTeacher: {
+                            name: "Sarah Lim"
+                        }
+                    }, {
+                        level: 'Primary 3',
+                        name: "Primary 3 Science",
+                        formTeacher: {
+                            name: "Hannah Low"
+                        }
+                    }]
+                });
+            })
+        })
+    })
+    describe("POST /api/classes", () => {
+        describe("returns 200 status code ", () => {
+            test("and  creates a new class successfully", async () => {
+                const classes = [
+                    {
+                        level: 'Primary 2',
+                        name: "Primary 2 English",
+                        teacherEmail: "sarah@gmail.com"
+                    }]
+                await classRepo.save(classes);
+                const res = await request(app)
+                    .post("/classes")
+                    .send({level: "Primary 3", name: "Primary 3 Science", teacherEmail: "hannahloh@gmail.com"});
+                expect(res.status).toBe(201);
+                expect(res.body).toEqual({})
+                const savedClasses = await classRepo.find({
+                    select: {
+                        level: true,
+                        name: true,
+                        teacherEmail: true,
+                    }
+                })
+                expect(savedClasses.length).toEqual(2)
+                expect(savedClasses).toEqual(expect.arrayContaining([{
+                        level: 'Primary 2',
+                        name: "Primary 2 English",
+                        teacherEmail: "sarah@gmail.com"
+                    }, {
+                        level: 'Primary 3',
+                        name: "Primary 3 Science",
+                        teacherEmail: "hannahloh@gmail.com"
+                    }])
+                )
+            });
+        })
+        describe("returns 400 status code ", () => {
+            test("if there is an invalid email", async () => {
+                const res = await request(app)
+                    .post("/classes")
+                    .send({level: "Primary 3", name: "Primary 3 Science", teacherEmail: "invalid-email"});
+
+                expect(res.status).toBe(400);
+                expect(res.body.error).toBe("Invalid email address");
+            });
+
+            test("if there is an email that does not have gmail as domain", async () => {
+                const res = await request(app)
+                    .post("/classes")
+                    .send({level: "Primary 3", name: "Primary 3 Science", teacherEmail: "sarah@hotmail.com"});
+
+                expect(res.status).toBe(400);
+                expect(res.body.error).toBe("Invalid email address");
+            });
+
+            test("if teacher not found", async () => {
+                const res = await request(app)
+                    .post("/classes")
+                    .send({level: "Primary 2", name: "Primary 2 science", teacherEmail: "clarissaLim@gmail.com"});
+
+                expect(res.status).toBe(400);
+                expect(res.body.error).toBe("Form teacher not found");
+            });
+
+            test("if level is missing", async () => {
+                const res = await request(app)
+                    .post("/classes")
+                    .send({
+                        name: "Primary 2 English",
+                        teacherEmail: "sarah@gmail.com"
+                    });
+
+                expect(res.status).toBe(400);
+                expect(res.body.error).toBe("level is required");
+            });
+
+            test("if name is missing", async () => {
+                const res = await request(app)
+                    .post("/classes")
+                    .send({
+                        level: 'Primary 2',
+                        teacherEmail: "sarah@gmail.com"
+                    });
+
+                expect(res.status).toBe(400);
+                expect(res.body.error).toBe("name is required");
+            });
+
+            test("if teacherEmail is missing", async () => {
+                const res = await request(app)
+                    .post("/classes")
+                    .send({
+                        level: 'Primary 2',
+                        name: "Primary 2 English",
+                    });
+
+                expect(res.status).toBe(400);
+                expect(res.body.error).toBe("teacherEmail is required");
+            });
+
+            test("if teacher already has a class", async () => {
+                const classes = [
+                    {
+                        level: 'Primary 2',
+                        name: "Primary 2 English",
+                        teacherEmail: "sarah@gmail.com"
+                    }, {
+                        level: 'Primary 3',
+                        name: "Primary 3 Science",
+                        teacherEmail: "hannahloh@gmail.com"
+                    }]
+                await classRepo.save(classes);
+                const res = await request(app)
+                    .post("/classes")
+                    .send({level: "Primary 4", name: "Primary 4 Science", teacherEmail: "hannahloh@gmail.com"});
+
+                expect(res.status).toBe(400);
+                expect(res.body.error).toBe("Selected teacher already has a class");
+            });
+        })
+    })
+});
---
+++ backend/src/routes/teachers/teachers.test.js
@@ -0,0 +1,263 @@
+import request from "supertest";
+import express from "express";
+import AppDataSource from "../../datasource.js";
+import TeacherEntity from "../../typeorm/entities/teacher.entity.js";
+import teachersRouter from "./teachers.js";
+import {IsNull, Not} from "typeorm";
+import ClassEntity from "../../typeorm/entities/class.entity.js";
+
+jest.mock("../../typeorm/logger/winston.js", () => ({
+    log: jest.fn()
+}));
+
+let app;
+
+beforeAll(async () => {
+    app = express();
+    app.use(express.json());
+    app.use("/teachers", teachersRouter);
+
+    await AppDataSource.initialize();
+    await AppDataSource.getRepository(ClassEntity).delete({id: Not(IsNull())});
+    await AppDataSource.getRepository(TeacherEntity).delete({id: Not(IsNull())});
+});
+
+afterEach(async () => {
+    await AppDataSource.getRepository(ClassEntity).delete({id: Not(IsNull())});
+    await AppDataSource.getRepository(TeacherEntity).delete({id: Not(IsNull())});
+});
+
+afterAll(async () => {
+    await AppDataSource.destroy();
+});
+
+describe("Teachers API", () => {
+    const teacherRepo = AppDataSource.getRepository(TeacherEntity);
+    describe("GET /teachers", () => {
+        describe("should return status code 200", () => {
+            test("and an empty array when no teachers are saved in the db", async () => {
+                const res = await request(app).get("/teachers");
+                expect(res.status).toBe(200);
+                expect(res.body).toEqual({data: []});
+            });
+
+            test("and an array of teachers if there are any teachers saved in the db", async () => {
+                const teachers = [
+                    {name: "Nicole Tan", email: "nicole@gmail.com", subject: "English", contactNumber: "61234567"},
+                    {name: "Lydia Lee", email: "lydia@gmail.com", subject: "Science", contactNumber: "67895132"}
+                ];
+                await teacherRepo.save(teachers);
+                const res = await request(app).get("/teachers");
+                expect(res.status).toBe(200);
+                expect(res.body).toEqual(
+                    {
+                        data: expect.arrayContaining([
+                            {
+                                name: "Nicole Tan",
+                                email: "nicole@gmail.com",
+                                subject: "English",
+                                contactNumber: "61234567"
+                            },
+                            {
+                                name: "Lydia Lee",
+                                email: "lydia@gmail.com",
+                                subject: "Science",
+                                contactNumber: "67895132"
+                            }
+                        ])
+                    })
+            });
+        });
+    })
+    describe("POST /teachers", () => {
+        describe("should return status code 200", () => {
+            test("and create a new teacher successfully", async () => {
+                const teachers = [
+                    {name: "Nicole Tan", email: "nicole@gmail.com", subject: "English", contactNumber: "61234567"},
+                    {name: "Lydia Lee", email: "lydia@gmail.com", subject: "Science", contactNumber: "67895132"}
+                ];
+                await teacherRepo.save(teachers);
+                const res = await request(app)
+                    .post("/teachers")
+                    .send({
+                        name: "Billy Tan",
+                        email: "billy@gmail.com",
+                        subject: "Physical Education",
+                        contactNumber: "61234568"
+                    });
+
+                expect(res.status).toBe(201);
+
+                const savedTeachers = await AppDataSource.getRepository(TeacherEntity).find({
+                    select: {
+                        name: true,
+                        email: true,
+                        subject: true,
+                        contactNumber: true,
+                    }
+                });
+                expect(savedTeachers.length).toBe(3);
+                expect(savedTeachers).toEqual(expect.arrayContaining([{
+                        name: "Nicole Tan",
+                        email: "nicole@gmail.com",
+                        subject: "English",
+                        contactNumber: "61234567"
+                    }, {
+                        name: "Lydia Lee",
+                        email: "lydia@gmail.com",
+                        subject: "Science",
+                        contactNumber: "67895132"
+                    }, {
+                        name: "Billy Tan",
+                        email: "billy@gmail.com",
+                        subject: "Physical Education",
+                        contactNumber: "61234568"
+                    }
+                    ])
+                )
+            });
+        })
+        describe("should return status code 400", () => {
+            test("should return 400 for invalid email", async () => {
+                const res = await request(app)
+                    .post("/teachers")
+                    .send({
+                        name: "Nicole Tan",
+                        email: "invalid-email",
+                        subject: "English",
+                        contactNumber: "6123 4567"
+                    });
+                expect(res.status).toBe(400);
+                expect(res.body.error).toBe("Invalid email address");
+            });
+            test("should return 400 for email that does not have gmail as domain", async () => {
+                const res = await request(app)
+                    .post("/teachers")
+                    .send({
+                        name: "Nicole Tan",
+                        email: "nicole@gov.sg",
+                        subject: "English",
+                        contactNumber: "6123 4567"
+                    });
+                expect(res.status).toBe(400);
+                expect(res.body.error).toBe("Invalid email address");
+            });
+
+            test("if name is missing", async () => {
+                const res = await request(app)
+                    .post("/teachers")
+                    .send({
+                        email: "nicole@gmail.com",
+                        subject: "English",
+                        contactNumber: "6123 4567"
+                    });
+                expect(res.status).toBe(400);
+                expect(res.body.error).toBe("name is required");
+            });
+
+            test("if email is missing", async () => {
+                const res = await request(app)
+                    .post("/teachers")
+                    .send({
+                        name: "Nicole Tan",
+                        subject: "English",
+                        contactNumber: "6123 4567"
+                    });
+                expect(res.status).toBe(400);
+                expect(res.body.error).toBe("email is required");
+            });
+
+            test("if subject is missing", async () => {
+                const res = await request(app)
+                    .post("/teachers")
+                    .send({
+                        name: "Nicole Tan",
+                        email: "nicole@gmail.com",
+                        contactNumber: "6123 4567"
+                    });
+                expect(res.status).toBe(400);
+                expect(res.body.error).toBe("subject is required");
+            });
+
+            test("if contactNumber is missing", async () => {
+                const res = await request(app)
+                    .post("/teachers")
+                    .send({
+                        name: "Nicole Tan",
+                        email: "nicole@gmail.com",
+                        subject: "English",
+                    });
+                expect(res.status).toBe(400);
+                expect(res.body.error).toBe("contactNumber is required");
+            });
+
+            test("if email already exists", async () => {
+                await teacherRepo.save({
+                    name: "Nicole Tan",
+                    email: "nicole@gmail.com",
+                    subject: "English",
+                    contactNumber: "61234567"
+                });
+
+                const res = await request(app)
+                    .post("/teachers")
+                    .send({
+                        name: "Sarah Tan",
+                        email: "nicole@gmail.com",
+                        subject: "Mathematics",
+                        contactNumber: "61234567"
+                    });
+
+                expect(res.status).toBe(400);
+                expect(res.body.error).toBe("Email already exists");
+            });
+
+            test("if contactNumber already exists", async () => {
+                await teacherRepo.save({
+                    name: "Nicole Tan",
+                    email: "nicole@gmail.com",
+                    subject: "English",
+                    contactNumber: "61234567"
+                });
+
+                const res = await request(app)
+                    .post("/teachers")
+                    .send({
+                        name: "Sarah Tan",
+                        email: "sarah@gmail.com",
+                        subject: "Mathematics",
+                        contactNumber: "61234567"
+                    });
+
+                expect(res.status).toBe(400);
+                expect(res.body.error).toBe("Contact number already exists");
+            });
+
+            test("for invalid contact number that is in the wrong format", async () => {
+                const res = await request(app)
+                    .post("/teachers")
+                    .send({
+                        name: "Nicole Tan",
+                        email: "nicole@gmail.com",
+                        subject: "English",
+                        contactNumber: "123"
+                    });
+                expect(res.status).toBe(400);
+                expect(res.body.error).toBe("Contact number must start with 6 and be exactly 8 digits");
+            });
+
+            test("for invalid contact number that is does not start with 6", async () => {
+                const res = await request(app)
+                    .post("/teachers")
+                    .send({
+                        name: "Nicole Tan",
+                        email: "nicole@gmail.com",
+                        subject: "English",
+                        contactNumber: "123"
+                    });
+                expect(res.status).toBe(400);
+                expect(res.body.error).toBe("Contact number must start with 6 and be exactly 8 digits");
+            });
+        })
+    });
+});
